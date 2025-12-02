315c79021ceb8312fe64b95c1f8934734f551b53
Christoffer Artmann <artgaard@gmail.com>
2025-12-02 13:58:42 +0100

    chore: Set up Eslint.

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import eslint from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off'
+    }
+  },
+  {
+    ignores: ['dist/']
+  }
+)
